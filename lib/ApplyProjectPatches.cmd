6b7d148dae12532df1035e49beaac6c343a67fe2
Fir <834772509@qq.com>
2026-04-19 15:35:52 +0800

    修复: 更新 ApplyProjectPatches.cmd 中的项目路径处理逻辑，确保路径正确解析

--- lib/ApplyProjectPatches.cmd
+++ lib/ApplyProjectPatches.cmd
@@ -1,5 +1,6 @@
 if "x%~1"=="x" goto :EOF
-set "project_path=%~1"
+set "project_path=%~f1"
+if "%project_path:~-1%"=="\" set "project_path=%project_path:~0,-1%"
 
 for /r "%project_path%" %%i in (.) do (
     if exist "%%~fi\main.cmd" (
@@ -11,10 +12,19 @@ for /r "%project_path%" %%i in (.) do (
 )
 
 for /r "%project_path%" %%i in (.) do (
-    if exist "%%~fi\last.cmd" (
-        echo \033[93;46m [执行] %%~fi\last.cmd | CmdColor.exe
-        pushd "%%~fi"
-        call "%%~fi\last.cmd"
-        popd
+    if /i not "%%~fi"=="%project_path%" (
+        if exist "%%~fi\last.cmd" (
+            echo \033[93;46m [执行] %%~fi\last.cmd | CmdColor.exe
+            pushd "%%~fi"
+            call "%%~fi\last.cmd"
+            popd
+        )
     )
+)
+
+if exist "%project_path%\last.cmd" (
+    echo \033[93;46m [执行] %project_path%\last.cmd | CmdColor.exe
+    pushd "%project_path%"
+    call "%project_path%\last.cmd"
+    popd
 )
